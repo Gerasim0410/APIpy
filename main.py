3eb75b8694c5560900136943fcea923cb903f021
Gerasim0410 <tyurina0410@gmail.com>
2025-11-06 23:16:55 +0300

    new model -06/11

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pydantic import BaseModel
-import numpy as np
+import pandas as pd
 import joblib
 
 app = FastAPI(root_path="/APIpy")
-model = joblib.load("age_model_svr.pkl")
 
-class FeatureSet(BaseModel):
-    cam_aver_time: float
-    cam_aver_abs_delta: float
-    math_aver_time: float
-    math_part_correct: float
-    mem_aver_time: float
-    mem_part_correct: float
-    mun_aver_time: float
-    str_aver_time: float
-    str_part_correct: float
-    swa_aver_time: float
-    swa_part_correct: float
-    sex: int
+model = joblib.load("Bagging_age_predictor.joblib")
+
+class Features(BaseModel):
+    swallow_time_red: float
+    munster_mean_attempt_time: float
+    stroop_var_attempt_time: float
 
 @app.post("/predict")
-async def predict(features: FeatureSet):
-    order = [
-        'cam_aver_time', 'cam_aver_abs_delta',
-        'math_aver_time', 'math_part_correct',
-        'mem_aver_time', 'mem_part_correct',
-        'mun_aver_time',
-        'str_aver_time', 'str_part_correct',
-        'swa_aver_time', 'swa_part_correct',
-        'sex'
-    ]
-    x = np.array([[getattr(features, key) for key in order]])
-    age = float(model.predict(x)[0])
-    return { "age": age }
+async def predict(features: Features):
+    data = pd.DataFrame([features.dict()])
+    pred = float(model.predict(data)[0])
+    return {"age": pred}
